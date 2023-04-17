26ec1d4a6b3d6fdc82aa93788dcccb28763217da
minhnguyen0910 <minhn091099@gmail.com>
2023-04-17 08:58:49 +0700

    Revert "Minh update admin form and cart"
    This reverts commit 756931fd7b59f26d38143429722ed6824b38c442.

--- watch_shop/src/main/java/com/example/watch_shop/repository/IOrderWatchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.watch_shop.repository;
-
-import com.example.watch_shop.model.OrderWatch;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface IOrderWatchRepository extends PagingAndSortingRepository<OrderWatch,Integer> {
-}
